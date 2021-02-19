6f48241f0a902418b97e44eda3102942261557dc
codydev901 <codydev901@gmail.com>
2021-02-18 22:32:13 -0500

    add cl arg

--- src/run_yolo.py
+++ src/run_yolo.py
@@ -1,3 +1,4 @@
+import sys
 from models.yolo_csv import YoloCSV
 from models.yolo_tree import YoloTree
 from models.yolo_plot import YoloPlot
@@ -21,8 +22,15 @@ g = {"a": ["d"],
 
 def main():
 
+    trap_num = 1
+    t_stop = 20
+
+    if len(sys.argv) == 3:
+        trap_num = int(sys.argv[1])
+        t_stop = int(sys.argv[2])
+
     yolo = YoloCSV("data/FT_BC8_yolo_short.csv")
-    graph_dict, yolo_trap_time = yolo.to_graph_dict(trap_num=1, t_stop=40)
+    graph_dict, yolo_trap_time = yolo.to_graph_dict(trap_num=trap_num, t_stop=t_stop)
     print(graph_dict)
     print("Tree Start")
     tree = YoloTree(graph_dict)

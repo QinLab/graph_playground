256b5352328567245b29bf7a9d51368a2e57a3b6
codydev901 <codydev901@gmail.com>
2021-03-17 21:54:53 -0400

    forgot from last commit

--- src/run_yolo.py
+++ src/run_yolo.py
@@ -1,8 +1,11 @@
 import sys
 import os
+from models.yolo_csv_two import YoloCSVTwo
 from models.yolo_csv import YoloCSV
+from models.yolo_tree_two import YoloTreeTwo
 from models.yolo_tree import YoloTree
-from models.yolo_plot import YoloPlot
+from models.yolo_plot_two import YoloPlotTwo
+from models.test_anytree import run_any_tree
 
 """
 Doc Doc Doc
@@ -23,18 +26,21 @@ def main():
         trap_num = int(sys.argv[1])
         t_stop = int(sys.argv[2])
 
-    yolo = YoloCSV("data/FT_BC8_yolo_short.csv")
+    yolo = YoloCSVTwo("data/FT_BC8_yolo_short.csv")
     graph_info = yolo.to_graph_info(trap_num=trap_num, t_stop=t_stop)
-    print("Graph Dict")
-    print(graph_info)
-    print(graph_info["check_data"])
-    tree = YoloTree(graph_info)
+
+    # print("Graph Info")
+    # print(graph_info)
+    # print("*")
+
+    tree = YoloTreeTwo(graph_info)
+
     print("Tree Dict")
     print(tree.tree_dict)
     print("Tree Info")
     print(tree.tree_info)
 
-    plot = YoloPlot(tree, graph_info)
+    plot = YoloPlotTwo(tree, graph_info)
     plot.show()
 
 
